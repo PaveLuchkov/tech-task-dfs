a37a58f2fd1d57efdf31898348ae5cef0fecfb32
paveluckov <sliderlaad222@gmail.com>
2025-09-02 12:12:03 +0500

    Добавлены файлы конфигурации и предобработки данных с параметрами очистки, YAKE и TF-IDF.

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# Параметры очистки данных
+MIN_WORD_COUNT = 25
+MAX_AVG_WORD_LEN = 15
+
+# Параметры для YAKE
+YAKE_LANGUAGE = "ru"
+YAKE_MAX_NGRAM_SIZE = 3
+YAKE_NUM_KEYWORDS = 7
+
+# Параметры для TF-IDF
+TFIDF_NGRAM_RANGE = (1, 3)
+TOP_N_SEARCH = 5
