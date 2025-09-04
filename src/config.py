0f9fff42b3ece84160a6599222594b564d43fbf6
paveluckov <sliderlaad222@gmail.com>
2025-09-04 14:22:27 +0500

    Добавлены параметры для модели и настройки FAISS в конфигурацию

--- src/config.py
+++ src/config.py
@@ -10,3 +10,8 @@ YAKE_NUM_KEYWORDS = 7
 # Параметры для TF-IDF
 TFIDF_NGRAM_RANGE = (1, 3)
 TOP_N_SEARCH = 5
+
+DEFAULT_MODEL_NAME = "ai-forever/FRIDA"
+DEFAULT_EMBEDDING_PATH = "../data/embeddings.npy"
+DEFAULT_FAISS_INDEX_PATH = "../data/faiss_index.bin"
+DEFAULT_BATCH_SIZE = 64
